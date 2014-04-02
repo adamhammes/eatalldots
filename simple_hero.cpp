06675854da627d2e94a4667ffe62742f59d1546b
adamhammes <adamhammes@centetune.com>
2014-04-01 23:19:08 -0500

    Finished Djikstra's Algorithm, added input checking

--- simple_hero.cpp
+++ simple_hero.cpp
@@ -49,7 +49,7 @@ void Simple_Hero::make_graph( GraphMap* map ) {
 
 Vertex& Simple_Hero::get_vertex( int x, int y ) {
 	if( !made_graph ) {
-		printf("Called get_vertex without initializing graph\n");
+		printf( "Called get_vertex without initializing graph\n" );
 		exit(0);
 	}
 
@@ -70,6 +70,17 @@ int Simple_Hero::select_neighbor( GraphMap* map, int x, int y ) {
 int Simple_Hero::where_go( GraphMap* map, int x1, int y1, int x2, int y2 ) {
 	queue<Vertex> v_q;
 
+	if( x1 < 0 || y1 < 0 ) {
+		printf( "Invalid coordinates for source vertex in where_go - (%d, %d)\n", x1, y1 );
+		exit(0);
+	}
+
+	if( x2 < 0 || y2 < 0 ) {
+		printf( "Invalid coordinates for dest vertex in where_go - (%d, %d)\n", x2, y2 );
+		exit(0);
+	}
+
+
 	Vertex source ( x1, y1 );
 	Vertex target ( x2, y2 );
 	Vertex popped, temp;
@@ -80,7 +91,6 @@ int Simple_Hero::where_go( GraphMap* map, int x1, int y1, int x2, int y2 ) {
 	while( !v_q.empty() ) {
 		popped = v_q.front();
 
-		if( popped == target ) break;
 
 		if( popped.visited ) {
 			v_q.pop();
@@ -92,13 +102,37 @@ int Simple_Hero::where_go( GraphMap* map, int x1, int y1, int x2, int y2 ) {
 			map->getNeighbor( popped.x, popped.y, i, a, b );
 			temp = get_vertex( a, b );
 			temp.prev = &popped;
-			temp.visited = true;
-			v_q.push( temp );
+
+			if( temp == target ) {
+				break;
+			}
+
+			if( !temp.visited ) {
+				temp.visited = true;
+				v_q.push( temp );
+			}
 		}
+
+		v_q.pop();
 		
 	}
 
-	return 0;
+	while( !(*temp.prev == source ) ) {
+		temp = *temp.prev;
+	}
+
+	for( int i = 0; i < map->getNumNeighbors( temp.prev->x, temp.prev->y ); i++ ) {
+		int a, b;
+		map->getNeighbor( temp.prev->x, temp.prev->y, i, a, b );
+		Vertex check( a, b );
+		
+		if( check == source ) {
+			return i;
+		}
+	}	
+
+
+	return -1;
 }
 
 
