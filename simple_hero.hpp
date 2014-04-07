3ee5f90e1fba73a266b2571ad8866d83d47cfc85
adamhammes <adamhammes@centetune.com>
2014-04-07 12:26:26 -0500

    Added definitions

--- simple_hero.hpp
+++ simple_hero.hpp
@@ -13,12 +13,17 @@
 using namespace std;
 
 class Simple_Hero : public Actor {
-
+	private:
 	protected: 
 	public:
 		int graph_width;
 		int graph_height;
 		bool made_graph;
+
+
+		vector< Vertex > actor_list;
+		vector< vector< Vertex > > graph;
+		vector< Vertex > ranked_actors;
 
 
 		Simple_Hero( int type );
@@ -32,15 +37,15 @@ class Simple_Hero : public Actor {
 		void update_vertices( GraphMap* map );
 		Vertex& get_vertex( int x, int y );	
 		void make_graph( GraphMap* map );
+		void rank_actors( GraphMap* map, int x_start, int y_start );
 
 
 
-		vector< Vertex > actor_list;
-		vector< vector< Vertex > > graph;
 
 
 		int find_path( Vertex v );
 		int where_go( GraphMap* map, int x1, int y1, int x2, int y2 );
+
 };
 
 #endif
