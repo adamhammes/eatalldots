b94a75b15f6598347dae0c325fed196904599d38
adamhammes <adamhammes@centetune.com>
2014-04-23 23:47:29 -0500

    Finally done setting up

---
+++ powerup.cpp
@@ -0,0 +1,26 @@
+#include "powerup.hpp"
+using namespace std;
+
+Powerup::Powerup( int type ): Actor( type ) {}
+
+Powerup* Powerup::duplicate() {
+	return new Powerup( this->getType() );
+}
+
+const char*  Powerup::getActorId() { 	
+	return "simplehero"; 
+}
+
+const char* Powerup::getNetId() {
+	return "hammesa"; 
+}
+
+
+bool Powerup::visited( const Vertex& v ) const {
+	return v_set.find( v ) != v_set.end();
+}
+
+size_t Powerup::dist( const Vertex& v) {
+	return distances[v];
+}
+
---
+++ powerup.hpp
@@ -0,0 +1,30 @@
+#ifndef POWERUP_HPP_
+#define POWERUP_HPP_
+
+#include "GraphMap.hpp"
+#include "vertex.hpp"
+#include "Actor.hpp"
+#include <unordered_set>
+#include <unordered_map>
+#include <functional>
+
+using namespace std;
+
+class Powerup : public Actor {
+	private:
+		unordered_set< Vertex, hash_vertex > v_set;
+		unordered_map< Vertex, int, hash_vertex > distances;
+
+		bool visited( const Vertex& v ) const;
+		size_t dist( const Vertex& v );
+	public:
+		Powerup( int type );
+		Powerup* duplicate();
+
+		const char* getActorId();
+		const char* getNetId();
+		int selectNeighbor( GraphMap* map, int x, int y );
+};
+
+
+#endif
