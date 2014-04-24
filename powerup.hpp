e02f126ab8ddd4d408b2d2d17182992d46016d8c
adamhammes <adamhammes@centetune.com>
2014-04-24 15:32:49 -0500

    removed dist()

--- powerup.hpp
+++ powerup.hpp
@@ -6,17 +6,21 @@
 #include "Actor.hpp"
 #include <unordered_set>
 #include <unordered_map>
+#include <queue>
+#include <vector>
 
 
 using namespace std;
 
 class Powerup : public Actor {
 	private: 
+		GraphMap* map;
 		unordered_set< Vertex, hash_vertex > v_set;
-		unordered_map< Vertex, int, hash_vertex > distances;
+		unordered_map< Vertex, int, hash_vertex > dist;
 
 		bool visited( const Vertex& v ) const;
 		int& dist( const Vertex& v );
+
 	public:
 		Powerup( int type );
 		int selectNeighbor( GraphMap* map, int x, int y );
@@ -24,5 +28,7 @@ class Powerup : public Actor {
 
 		const char* getActorId();
 		const char* getNetId();
+
+		void rank_nodes( int x, int y );
 };
 #endif
